5717c6af6f77ff36ba75bbc06548db68d353df94
Zachary Gannon <zachgannon93@gmail.com>
2017-09-19 10:17:00 -0400

    Lets see if this works...

--- personal_page/settings.py
+++ personal_page/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 
 # Logging
 
