956a0014b269e6a4bd03fd0df618d948b32d8c50
Zachary Gannon <zachgannon93@gmail.com>
2017-09-27 12:22:26 -0400

    Changed font. Updated sidebar

--- home/urls.py
+++ home/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+# from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     url(r'^wip/', views.wip_page, name='wip'),
+    url(r'^coding/', views.coding_page, name='coding'),
+    url(r'^music/', views.music_page, name='music'),
+    url(r'^gaming/', views.gaming_page, name='gaming'),
+    url(r'^contact/', views.contact_page, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
-from django.core.exceptions import ValidationError
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def home_page(request):
@@ -9,3 +8,19 @@ def home_page(request):
 
 def wip_page(request):
     return render(request, 'wip.html')
+
+
+def gaming_page(request):
+    return render(request, 'gaming.html')
+
+
+def music_page(request):
+    return render(request, 'music.html')
+
+
+def contact_page(request):
+    return render(request, 'contact.html')
+
+
+def coding_page(request):
+    return render(request, 'coding.html')
