b2a4f761b503dd6fab094e83dfd5a97b1be6e09c
Zachary Gannon <zachgannon93@gmail.com>
2017-10-06 15:59:19 -0400

    Overlay test

---
+++ home/static/js/overlay.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openOverlay() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeOverlay() {
+    document.getElementById("overlay").style.display = "none";
+}
