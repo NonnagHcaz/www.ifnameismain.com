5f4cecc963656549ab36e3f8060ee703d3c1ded2
Zachary Gannon <zachgannon93@gmail.com>
2017-10-10 10:08:11 -0400

    Replaced janky typewriter effect with TinyTyper.

---
+++ home/static/js/custom_typer.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    var el       = document.querySelector('#message')
+    var options  = {
+        text: (
+            "Initializing content...\n" +
+            "Content initialized.\n\n" +
+            "Enter username:\n" +
+            "$> Retr0\n" +
+            "Enter password:\n" +
+            "$> *****************\n\n" +
+            "Welcome Retr0.\n" +
+            "$> "
+            ),
+        textSpeed: 75,
+        blinkSpeed: 0.05,
+        cursor: "_"
+    }
+    var instance = new tinytyper(el, options)
+});
