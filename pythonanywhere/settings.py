be789c2874d5ab350fd904438c60fa9794984f4e
Zachary Gannon <zachgannon93@gmail.com>
2018-01-05 15:36:04 -0500

    Add new domain to allowed hosts

--- pythonanywhere/settings.py
+++ pythonanywhere/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'www.ifnameismain.com']
 
 
 # Application definition
