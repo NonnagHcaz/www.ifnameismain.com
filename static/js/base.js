0255a057b55fc745a20681353fb44e2533b683f5
Zachary Gannon <zachgannon93@gmail.com>
2018-01-16 15:55:57 -0500

    Testing theme switching

--- static/js/base.js
+++ static/js/base.js
@@ -1,2 +1,22 @@
 function pluralize(num, singular, plural){
     return''+ num+' '+(num===1?singular:plural);}
+
+function changeTheme(filename) {
+    themeBaseDir = "css/gkit_css/themes/"
+    newstylesheet = (
+        "{% static '" + themeBaseDir + "style_" + filename + ".css' %}");
+
+    if ($("#dynamic_css").length == 0) {
+        $("head").append("<link>")
+        css = $("head").children(":last");
+
+        css.attr({
+          id: "dynamic_css",
+          rel:  "stylesheet",
+          type: "text/css",
+          href: newstylesheet
+        });
+    } else {
+        $("#dynamic_css").attr("href",newstylesheet);
+    }
+}
